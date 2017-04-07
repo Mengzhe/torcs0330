af62548bf3ce554bca8ce8729c28c0bda60a4d56
Mengzhe <mengzhehuang@gmail.com>
2017-04-07 09:17:18 -0400

    gear and brake 1

--- torcs-1.3.7/src/drivers/batman/batman.cpp
+++ torcs-1.3.7/src/drivers/batman/batman.cpp
@@ -1,8 +1,8 @@
 /***************************************************************************
 
-    file                 : batman.cpp
-    created              : Sat Apr 1 13:05:42 EDT 2017
-    copyright            : (C) 2002 mh
+    file                 : bt.cpp
+    created              : Thu Dec 12 02:34:31 CET 2002
+    copyright            : (C) 2002 Bernhard Wymann
 
  ***************************************************************************/
 
@@ -20,26 +20,33 @@
 #endif
 
 #include <stdio.h>
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-#include <tgf.h> 
-#include <track.h> 
-#include <car.h> 
-#include <raceman.h> 
+#include <tgf.h>
+#include <track.h>
+#include <car.h>
+#include <raceman.h>
 #include <robottools.h>
 #include <robot.h>
 
-static tTrack	*curTrack;
+#include "driver.h"
 
-static void initTrack(int index, tTrack* track, void *carHandle, void **carParmHandle, tSituation *s); 
-static void newrace(int index, tCarElt* car, tSituation *s); 
-static void drive(int index, tCarElt* car, tSituation *s); 
-static void endrace(int index, tCarElt *car, tSituation *s);
+#define BUFSIZE 20
+#define NBBOTS 3
+
+static char *botname[NBBOTS];
+static Driver *driver[NBBOTS];
+
+static void initTrack(int index, tTrack* track,
+                      void *carHandle, void **carParmHandle, tSituation *s);
+static void newRace(int index, tCarElt* car, tSituation *s);
+static void drive(int index, tCarElt* car, tSituation *s);
+static int  pitcmd(int index, tCarElt* car, tSituation *s);
 static void shutdown(int index);
-static int  InitFuncPt(int index, void *pt); 
-
+static int InitFuncPt(int index, void *pt);
+static void endRace(int index, tCarElt *car, tSituation *s);
 
 /* 
  * Module entry point  
@@ -47,114 +54,81 @@ static int  InitFuncPt(int index, void *pt);
 extern "C" int 
 batman(tModInfo *modInfo) 
 {
-    memset(modInfo, 0, 10*sizeof(tModInfo));
+	char buffer[BUFSIZE];
+    int i;    
 
-    modInfo->name    = strdup("batman");		/* name of the module (short) */
-    modInfo->desc    = strdup("");	/* description of the module (can be long) */
-    modInfo->fctInit = InitFuncPt;		/* init function */
-    modInfo->gfId    = ROB_IDENT;		/* supported framework version */
-    modInfo->index   = 1;
+	/* clear all structures */	
+	memset(modInfo, 0, 10*sizeof(tModInfo));
 
+
+    for (i=0; i<NBBOTS; i++)
+	{
+		sprintf(buffer, "batman %d", i+1);
+		botname[i] = strdup(buffer);		/* store pointer */
+    	modInfo[i].name    = botname[i];	/* name of the module (short) */
+   		modInfo[i].desc    = strdup("");	/* description of the module (can be long) */
+    	modInfo[i].fctInit = InitFuncPt;	/* init function */
+    	modInfo[i].gfId    = ROB_IDENT;		/* supported framework version */
+    	modInfo[i].index   = i;
+	}
     return 0; 
-} 
+}
 
 /* Module interface initialization. */
-static int 
-InitFuncPt(int index, void *pt) 
-{ 
-    tRobotItf *itf  = (tRobotItf *)pt; 
+static int InitFuncPt(int index, void *pt)
+{
+    tRobotItf *itf = (tRobotItf *)pt;
 
-    itf->rbNewTrack = initTrack; /* Give the robot the track view called */ 
-				 /* for every track change or new race */ 
-    itf->rbNewRace  = newrace; 	 /* Start a new race */
-    itf->rbDrive    = drive;	 /* Drive during race */
-    itf->rbPitCmd   = NULL;
-    itf->rbEndRace  = endrace;	 /* End of the current race */
-    itf->rbShutdown = shutdown;	 /* Called before the module is unloaded */
-    itf->index      = index; 	 /* Index used if multiple interfaces */
-    return 0; 
-} 
+    /* create robot instance for index */
+    driver[index] = new Driver(index);
+    itf->rbNewTrack = initTrack; /* Give the robot the track view called */
+    itf->rbNewRace  = newRace;   /* Start a new race */
+    itf->rbDrive    = drive;     /* Drive during race */
+    itf->rbPitCmd   = pitcmd;    /* Pit commands */
+    itf->rbEndRace  = endRace;   /* End of the current race */
+    itf->rbShutdown = shutdown;  /* Called before the module is unloaded */
+    itf->index      = index;     /* Index used if multiple interfaces */
+    return 0;
+}
 
 /* Called for every track change or new race. */
-static void  
-initTrack(int index, tTrack* track, void *carHandle, void **carParmHandle, tSituation *s) 
-{ 
-    curTrack = track;
-    *carParmHandle = NULL; 
-} 
+static void initTrack(int index, tTrack* track, void *carHandle,
+                      void **carParmHandle, tSituation *s)
+{
+    driver[index]->initTrack(track, carHandle, carParmHandle, s);
+}
 
 /* Start a new race. */
-static void  
-newrace(int index, tCarElt* car, tSituation *s) 
-{ 
-} 
-
-//counter 
-static int stuck = 0;
-/* Check if the car is stuck */
-bool isStuck(tCarElt* car)
+static void newRace(int index, tCarElt* car, tSituation *s)
 {
-    float angle = RtTrackSideTgAngleL(&(car->_trkPos)) - car->_yaw;
-    NORM_PI_PI(angle);
-
-    //angle smaller than 30 degrees?
-    if(fabs(angle) < 30.0/180.0*PI)
-    {
-        stuck = 0;
-        return false;
-    }
-    if(stuck < 100)
-    {
-        stuck++;
-        return false;
-    }
-    else
-    {
-        return true;
-    }
+    driver[index]->newRace(car, s);
 }
 
 
 /* Drive during race. */
-static void  
-drive(int index, tCarElt* car, tSituation *s) 
-{ 
-    memset((void *)&car->ctrl, 0, sizeof(tCarCtrl)); 
-    
-    float angle;
-    const float SC = 1.0;
+static void drive(int index, tCarElt* car, tSituation *s)
+{
+    driver[index]->drive(car, s);
+}
 
-    if(isStuck(car))
-    {
-        angle = -RtTrackSideTgAngleL(&(car->_trkPos)) - car->_yaw;
-        NORM_PI_PI(angle);
-        car->ctrl.steer = -angle/ car->_steerLock;  /*steering angle*/
-        car->ctrl.gear = -1;        /*reverse gear*/
-        car->ctrl.accelCmd = 0.3;   /*30% acceleraor pedal*/
-        car->ctrl.brakeCmd = 0.0; /* no brakes */ 
-    }
-    else
-    {
-        angle = RtTrackSideTgAngleL(&(car->_trkPos)) - car->_yaw;
-        NORM_PI_PI(angle);  
-        angle -= SC*car->_trkPos.toMiddle/ car->_trkPos.seg->width;
-        car->ctrl.steer = angle/ car->_steerLock;   /*steering angle*/
-        car->ctrl.gear = 2;     /*second gear*/
-        car->ctrl.accelCmd = 0.3;   /*30% acceleraor pedal*/
-        car->ctrl.brakeCmd = 0.0; /* no brakes */ 
-    }
-    
-}      
+
+/* Pitstop callback */
+static int pitcmd(int index, tCarElt* car, tSituation *s)
+{
+    return driver[index]->pitCommand(car, s);
+}
+
 
 /* End of the current race */
-static void
-endrace(int index, tCarElt *car, tSituation *s)
+static void endRace(int index, tCarElt *car, tSituation *s)
 {
+    driver[index]->endRace(car, s);
 }
+
 
 /* Called before the module is unloaded */
-static void
-shutdown(int index)
+static void shutdown(int index)
 {
+    free(botname[index]);
+    delete driver[index];
 }
-
---
+++ torcs-1.3.7/src/drivers/batman/driver.cpp
@@ -0,0 +1,169 @@
+#include "driver.h"
+
+// const float Driver::MAX_UNSTUCK_ANGLE = 30.0/180.0*PI;  /* [radians] */
+const float Driver::MAX_UNSTUCK_ANGLE = 20.0/180.0*PI;  /* [radians] */
+const float Driver::UNSTUCK_TIME_LIMIT = 2.0;           /* [s] */
+
+const float Driver::MAX_UNSTUCK_SPEED = 5.0;   /* [m/s] */
+const float Driver::MIN_UNSTUCK_DIST = 3.0;    /* [m] */
+
+const float Driver::G = 9.81;                  /* [m/(s*s)] */
+const float Driver::FULL_ACCEL_MARGIN = 1.0;   /* [m/s] */
+
+Driver::Driver(int index)
+{
+    INDEX = index;
+}
+
+
+/* Called for every track change or new race. */
+void Driver::initTrack(tTrack* t, void *carHandle, void **carParmHandle, tSituation *s)
+{
+    track = t;
+    *carParmHandle = NULL;
+}
+
+/* Start a new race. */
+void Driver::newRace(tCarElt* car, tSituation *s)
+{
+    MAX_UNSTUCK_COUNT = int(UNSTUCK_TIME_LIMIT/RCM_MAX_DT_ROBOTS);
+    stuck = 0;
+}
+
+/* Drive during race. */
+void Driver::drive(tCarElt* car, tSituation *s)
+{
+    update(car, s);
+
+    memset(&car->ctrl, 0, sizeof(tCarCtrl));
+
+    if (isStuck(car)) {
+        car->ctrl.steer = -angle / car->_steerLock;
+        car->ctrl.gear = -1; // reverse gear
+        // car->ctrl.accelCmd = 0.5; // 50% accelerator pedal
+        // car->ctrl.brakeCmd = 0.0; // no brakes
+        car->ctrl.brakeCmd = getBrake(car);
+        if (car->ctrl.brakeCmd == 0.0) {
+            car->ctrl.accelCmd = getAccel(car);
+        } else {
+            car->ctrl.accelCmd = 0.0;
+        }
+    } else {
+        float steerangle = angle - car->_trkPos.toMiddle/car->_trkPos.seg->width;
+
+        car->ctrl.steer = steerangle / car->_steerLock;
+        car->ctrl.gear = 3; // first gear
+        //car->ctrl.accelCmd = 0.3; // 30% accelerator pedal
+        //car->ctrl.brakeCmd = 0.0; // no brakes
+        car->ctrl.brakeCmd = getBrake(car);
+        if (car->ctrl.brakeCmd == 0.0) {
+            car->ctrl.accelCmd = getAccel(car);
+        } else {
+            car->ctrl.accelCmd = 0.0;
+        }
+    }
+}
+
+/* Set pitstop commands. */
+int Driver::pitCommand(tCarElt* car, tSituation *s)
+{
+    return ROB_PIT_IM; /* return immediately */
+}
+
+/* End of the current race */
+void Driver::endRace(tCarElt *car, tSituation *s)
+{
+}
+
+
+/* */
+/* Update my private data every timestep */
+void Driver::update(tCarElt* car, tSituation *s)
+{
+    trackangle = RtTrackSideTgAngleL(&(car->_trkPos));
+    angle = trackangle - car->_yaw;
+    NORM_PI_PI(angle);
+}
+
+/* Check if I'm stuck */
+bool Driver::isStuck(tCarElt* car)
+{
+    if (fabs(angle) > MAX_UNSTUCK_ANGLE &&
+        car->_speed_x < MAX_UNSTUCK_SPEED &&
+        fabs(car->_trkPos.toMiddle) > MIN_UNSTUCK_DIST) {
+        if (stuck > MAX_UNSTUCK_COUNT && car->_trkPos.toMiddle*angle < 0.0) {
+            return true;
+        } else {
+            stuck++;
+            return false;
+        }
+    } else {
+        stuck = 0;
+        return false;
+    }
+}
+
+/* Compute the allowed speed on a segment */
+float Driver::getAllowedSpeed(tTrackSeg *segment)
+{
+    if (segment->type == TR_STR) {
+        return FLT_MAX;
+    } else {
+        float mu = segment->surface->kFriction;
+        return sqrt(mu*G*segment->radius);
+    }
+}   
+
+/* Compute the length to the end of the segment */
+float Driver::getDistToSegEnd(tCarElt* car)
+{
+    if (car->_trkPos.seg->type == TR_STR) {
+        return car->_trkPos.seg->length - car->_trkPos.toStart;
+    } else {
+        return (car->_trkPos.seg->arc - car->_trkPos.toStart)*car->_trkPos.seg->radius;
+    }
+}   
+
+/* Compute fitting acceleration */
+float Driver::getAccel(tCarElt* car)
+{
+    float allowedspeed = getAllowedSpeed(car->_trkPos.seg);
+    float gr = car->_gearRatio[car->_gear + car->_gearOffset];
+    float rm = car->_enginerpmRedLine;
+    if (allowedspeed > car->_speed_x + FULL_ACCEL_MARGIN) {
+        return 1.0;
+    } else {
+        return allowedspeed/car->_wheelRadius(REAR_RGT)*gr /rm;
+    }
+}  
+
+
+/* Compute if need to brake */
+float Driver::getBrake(tCarElt* car)
+{
+    tTrackSeg *segptr = car->_trkPos.seg;
+    float currentspeedsqr = car->_speed_x*car->_speed_x;
+    float mu = segptr->surface->kFriction;
+    /* maxlookaheddist is the distance we have to check (formula with special case v2 = 0) */
+    float maxlookaheaddist = currentspeedsqr/(2.0*mu*G);
+    /* lookaheaddist holds the distance we have already checked */
+    float lookaheaddist = getDistToSegEnd(car);
+    /* Compute the allowed speed on the current segment.  */
+    float allowedspeed = getAllowedSpeed(segptr);
+    if (allowedspeed < car->_speed_x) return 1.0;
+    segptr = segptr->next;
+    while (lookaheaddist < maxlookaheaddist) {
+    allowedspeed = getAllowedSpeed(segptr);
+        if (allowedspeed < car->_speed_x) {
+            /* Compute the braking distance according to the formula */
+            float allowedspeedsqr = allowedspeed*allowedspeed;
+            float brakedist = (currentspeedsqr - allowedspeedsqr) / (2.0*mu*G);
+            if (brakedist > lookaheaddist) {
+            return 1.0;
+            }
+        }
+        lookaheaddist += segptr->length;
+        segptr = segptr->next;
+    }
+    return 0.0;
+} 
